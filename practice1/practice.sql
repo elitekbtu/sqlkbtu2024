32a8e9cdc57ccd0be0de398b290c10234bf926a7
elitekbtu <satbaldievst@mail.ru>
2024-09-18 01:44:08 +0500

    lab1 completed

--- practice1/practice.sql
+++ practice1/practice.sql
@@ -1,46 +1,27 @@
-drop database if exists lab1 ;
+drop database if exists lab1;
 create database lab1;
 \c lab1\
-
--- Удаляем таблицы, если они существуют
 drop table if exists users;
 drop table if exists tasks;
 
--- Создаем таблицу users
+create table users(
+    id serial,
+    firstname varchar(50),
+    lastname varchar(50)
+);
+alter table users add column isadmin int default 1 check(isadmin in(1, 0));
+alter table users alter column isadmin drop default;
+alter table users drop constraint users_isadmin_check;
+alter table users alter column isadmin type bool using isadmin::boolean;
+alter table users alter column isadmin set default false;
+alter table users add constraint uniq primary key(id);
 
-CREATE TABLE users(
-  id SERIAL,
-  firstname VARCHAR(50),
-  lastname VARCHAR(50)
+create table tasks(
+    id serial,
+    name varchar(50),
+    user_id INT
 );
 
--- SELECT * FROM users;
-
-ALTER TABLE users
-    ADD COLUMN isadmin INT;
-
-ALTER TABLE users
-    ALTER COLUMN isadmin TYPE BOOL USING isadmin::boolean;
-
-ALTER TABLE users
-    ALTER COLUMN isadmin SET DEFAULT FALSE;
-
--- INSERT INTO users VALUES (DEFAULT ,'Aza', 'Qwe', DEFAULT );
-
-ALTER TABLE users
-    ADD CONSTRAINT uniq PRIMARY KEY (id);
-
-CREATE TABLE tasks(
-  id SERIAL,
-  name VARCHAR(50),
-  user_id INT
-);
-
--- SELECT * FROM tasks;
-
-drop table if exists tasks;
-drop table if exists users;
-
--- Подключаемся к базе данных postgres и удаляем базу данных lab1
+drop table tasks;
 \c postgres
-drop database if exists lab1;
+drop database lab1;
