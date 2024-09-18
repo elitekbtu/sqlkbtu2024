03bf75ec51008e74c0c704f9f1ecb6b2acf9fd45
elitekbtu <satbaldievst@mail.ru>
2024-09-18 14:47:11 +0500

    lecture 3

--- lecture2/draft2.sql
+++ lecture2/draft2.sql
@@ -75,7 +75,44 @@ create table table3{
 insert into table3(name, price, weight) values("turar", 12, 123);
 select * from table3;
 
-insert into table3 values(default, "pepsi", 145, 5);
+insert into table3 values(default, "pepsi", 145, 55.5 double::integer);
 -- insert into table3 values(default, "pepsi", 145); weight will be null
 
 insert into table3 default values;
+
+
+-- lecture 3 second part 
+
+insert into table3 (name, price, weight) select name, price, weight from table3 where id=3;
+-- selecting from other table 
+
+insert into table3(name, price, weight) values("turar", 12, 123),
+values("turar", 12, 123), values("turar", 12, 123), values("turar", 12, 123) ;
+
+-- grouping insert 
+
+-- update change exists values 
+
+-- alias change table name in inside command for ex: table12345 you can change alias tb123;
+
+
+update table3 set price=100 where price is null;
+
+update table3 set price = price*1.2, name ="smth",  where price=200;
+
+update table3 set price = price*1.2, name ="smth",  where price=200;
+select * from table3; 
+-- sub 
+update table3 set weight = (select max(weight) from table3) where weight is null;
+
+update table3 set (weight, name) = (select max(weight), max(price) from table3) where weight is null;
+
+create table category(
+    id serial primary key, 
+    name varchar(200)
+);
+insert into category(name) values("ghjkl;", "ghjkl;'","ertyuio");
+
+-- deleting data 
+delete from table3 where category =1 ;
+delete from table using "another table"  
