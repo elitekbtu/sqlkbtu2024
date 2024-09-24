5dbe25a53b4948271eae547394c6f5df79ccc099
elitekbtu <satbaldievst@mail.ru>
2024-09-25 00:57:15 +0500

    lab2

--- draft3.sql
+++ draft3.sql
@@ -1,26 +1,63 @@
 drop database if exists lab2;
+--1
 create database lab2;
+
 \c lab2
+
 drop table if exists countries cascade;
-create table countries(
+--2
+create table countries (
     country_id serial primary key,
     country_name varchar(255),
-    region_id int, 
-    population int
+    region_id integer,
+    population integer
 );
-insert into countries(country_name, region_id, population) values("Usa", 1, 123456789);
-insert into countries(country_name) values("Greece");
-insert into countries(region_id) values(null);
-insert into countries(country_name, region_id, population) values("Dagestan", 15, 1234567), ("Russia", 12, 34567890), ("Crotia", 10, 5678909);
-alter table alter column country_name set default 'Kazakhstan';
-insert into countries(country_name) values(default);
+--3
+insert into countries (country_name, region_id, population)
+values ('canada', 1, 37590000);
+--4
+insert into countries (country_name)
+values ('mexico');
+--5
+insert into countries (country_name, region_id)
+values ('france', null);
+--6
+insert into countries (country_name, region_id, population)
+values
+    ('brazil', 3, 211000000),
+    ('germany', 4, 83000000),
+    ('japan', 5, 126000000);
+--7
+alter table countries
+alter column country_name set default 'kazakhstan';
+--8
+insert into countries (country_name, region_id, population)
+values (default,5, 18500000);
+--9
 insert into countries default values;
+--10
+drop table if exists countries_new;
 create table countries_new (like countries including all);
-insert into countries_new select * from countries;
-update countries set region_id = 1 where region_id=null;
-select country_name, population * 1.1 as "new population" from countries;
-delete from countries where population<100000;
-delete from countries_new where country_id in(select country_id from countries) returning *;
+--11
+insert into countries_new
+select * from countries;
+--12
+update countries
+set region_id = 1
+where region_id is null;
+--13
+select country_name,
+       population * 1.1 as "new population"
+from countries;
+--14
+delete from countries
+where population < 100000;
+--15
+delete from countries_new
+where country_id in (select country_id from countries)
+returning *;
+--16
 delete from countries
 returning *;
+
 \c postgres
--- practise2/practise.sql
+++ practise2/practise.sql
@@ -1,78 +1,63 @@
+drop database if exists lab2;
+--1
 create database lab2;
--- Создание новой базы данных с именем "lab2"
 
 \c lab2
--- Подключение к базе данных "lab2"
 
+drop table if exists countries cascade;
+--2
 create table countries (
     country_id serial primary key,
-    country_name varchar(255) not null,
+    country_name varchar(255),
     region_id integer,
     population integer
 );
--- Создание таблицы "countries" с полями:
--- 1. country_id: автоинкрементируемый первичный ключ (serial),
--- 2. country_name: имя страны (строка длиной до 255 символов), которое не может быть пустым,
--- 3. region_id: идентификатор региона (integer),
--- 4. population: численность населения (integer).
-
-insert into countries (country_name, region_id, population) 
+--3
+insert into countries (country_name, region_id, population)
 values ('canada', 1, 37590000);
--- Вставка строки для Канады с region_id = 1 и населением 37,590,000.
-
-insert into countries (country_id, country_name) 
-values (2, 'mexico');
--- Вставка строки для Мексики с указанным country_id = 2, но без численности населения и region_id (они будут NULL).
-
-insert into countries (country_name, region_id, population) 
-values ('france', null, 67000000);
--- Вставка строки для Франции с численностью населения 67,000,000. region_id будет NULL.
-
-insert into countries (country_name, region_id, population) 
-values ('brazil', 3, 211000000), 
-       ('germany', 2, 83000000), 
-       ('japan', 4, 126000000);
--- Вставка строк для Бразилии, Германии и Японии с соответствующими region_id и населением.
-
+--4
+insert into countries (country_name)
+values ('mexico');
+--5
+insert into countries (country_name, region_id)
+values ('france', null);
+--6
+insert into countries (country_name, region_id, population)
+values
+    ('brazil', 3, 211000000),
+    ('germany', 4, 83000000),
+    ('japan', 5, 126000000);
+--7
 alter table countries
 alter column country_name set default 'kazakhstan';
--- Изменение столбца country_name, добавление значения по умолчанию 'казахстан'.
-
-insert into countries (region_id, population) 
-values (5, 18500000);
--- Вставка новой строки, где указаны только region_id и численность населения. 
--- Имя страны будет установлено как 'казахстан' по умолчанию.
-
+--8
+insert into countries (country_name, region_id, population)
+values (default,5, 18500000);
+--9
 insert into countries default values;
--- Вставка строки с использованием всех значений по умолчанию. 
--- country_id будет автоинкрементирован, а country_name установлено на 'казахстан'.
-
+--10
+drop table if exists countries_new;
 create table countries_new (like countries including all);
--- Создание новой таблицы countries_new с той же структурой и ограничениями, что и у таблицы countries.
-
-insert into countries_new 
+--11
+insert into countries_new
 select * from countries;
--- Копирование всех строк из таблицы countries в таблицу countries_new.
-
+--12
 update countries
 set region_id = 1
 where region_id is null;
--- Обновление всех строк в таблице countries, где region_id равно NULL, устанавливая region_id = 1.
-
-select country_name, 
+--13
+select country_name,
        population * 1.1 as "new population"
 from countries;
--- Выбор названия страны и вычисленного значения населения (увеличенного на 10%) для каждой страны из таблицы countries.
-
+--14
 delete from countries
 where population < 100000;
--- Удаление строк из таблицы countries, где численность населения меньше 100,000.
-
+--15
 delete from countries_new
 where country_id in (select country_id from countries)
 returning *;
--- Удаление строк из таблицы countries_new, где country_id совпадает с country_id из таблицы countries. Возвращает удалённые строки.
-
+--16
 delete from countries
 returning *;
--- Удаление всех оставшихся строк из таблицы countries и возврат удалённых строк.
+
+\c postgres
