5ef52a912cbde0f23f0ca931719b5c533423df2d
elitekbtu <satbaldievst@mail.ru>
2024-10-09 12:42:47 +0500

    lab4

--- practice4/lab4.sql
+++ practice4/lab4.sql
@@ -1,76 +1,97 @@
-drop database if exists lab4;
-
 create database lab4;
-drop table if exists warehouses cascade;
-drop table if exists boxes cascade;
 
-create table warehouses(
-    code int,
-    location character varying (255),
-    capacity int
+create table warehouse(
+    code integer,
+    location varchar(255),
+    capacity integer
 );
 
-create table boxes (
+create table boxes(
     code character(4),
-    contents character varying (255),
+    contents varchar(255),
     value real,
-    warehouse int
+    warehouse integer
 );
 
-insert into warehouses (code, location, capacity) values
-(1,'chicago',3),
-(2,'chicago',4),
-(3,'new york',7),
-(4,'los angeles',2),
-(5,'san francisco',8);
 
-insert into boxes (code, contents, value, warehouse) values
-('omnt','rocks',180,3),
-('4h8p','rocks',250,1),
-('4rt3','scissors',190,4),
-('763h','rocks',200,1),
-('8jn6','papers',75,1),
-('8y6u','papers',50,3),
-('9j6f','papers',175,2),
-('ll08','rocks',140,4),
-('рон6','scissors',125,1),
-('p2t6','scissors',150,2),
-('tu55','papers',90,5);
+insert into warehouse(code, location, capacity)
+values
+(1,'Chicago',3),
+(2,'Chicago',4),
+(3,'New York',7),
+(4,'Los Angeles',2),
+(5,'San Francisco',8);
 
-select * from warehouses;
+
+insert into boxes(code, contents, value, warehouse)
+values
+('OMN7','Rocks',180,3),
+('4H8P','Rocks',250,1),
+('4RT3','Scissors',190,4),
+('763H','Rocks',200,1),
+('8JN6','Papers',75,1),
+('8Y6U','Papers',50,3),
+('9J6F','Papers',175,2),
+('LL08','Rocks',140,4),
+('РОН6','Scissors',125,1),
+('P2T6','Scissors',150,2),
+('TU55','Papers',90,5);
+
+--4
+select * from  warehouse;
 select * from boxes;
 
-select * from warehouses;
-
+--5
 select * from boxes
-where value > 150;
+where value>150;
 
-select distinct contents from boxes;
+--6
+select  distinct contents
+from boxes;
 
-select warehouse, count(*) as box_count
+
+--7
+select warehouse ,COUNT(*) as warehouse_count
 from boxes
 group by warehouse;
 
-select warehouse, count(*) as box_count
-from boxes
-group by warehouse
-having count(*) > 2;
 
-insert into warehouses (code, location, capacity)
-values (4, 'new york', 3);
+--8
+select warehouse,COUNT(*) as warehouse_count
+    from boxes
+    group by warehouse
+Having count(*)>2;
 
-insert into boxes (code, contents, value, warehouse)
-values ('h5rt', 'papers', 200, 2);
+
+--9
+insert into warehouse(code, location, capacity)
+values(3,'New York',3);
+
+select * from warehouse;
+select * from boxes;
+
+--10
+insert into boxes(code, contents, value, warehouse)
+values('H5RT','Papers',200,2);
+select * from boxes;
+
+
+
+--11
 
 update boxes
-set value = value * 0.85
-where value = (select value from boxes order by value desc offset 2 limit 1);
+set value=value*0.85
+where value=(select value  from boxes order by  value desc offset 2 limit 1);
 
-delete from boxes
-where value < 150;
 
+--12
 delete from boxes
-using warehouses
-where boxes.warehouse = warehouses.code
-and warehouses.location = 'new york'
-returning boxes.*;
+where value<150;
+
+
+--13
+delete from boxes
+using warehouse
+where boxes.warehouse=warehouse.code
+and warehouse.location='New York'
+returning *;
